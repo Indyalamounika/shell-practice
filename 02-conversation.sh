e0dd15adbc0203478295fea5c9790e092fc9406e
Indyalamounika <vedamouni01@gmail.com>
2025-09-19 02:18:15 +0530

    some message

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Mounika:: Hi Siva, How are you?"
+echo "Siva:: Hello Mounika, I am fine. How are you?"
+echo "Mounika:: What are you learning recently?"
+echo "Siva:: I am learning Shell scripting Mounika"
